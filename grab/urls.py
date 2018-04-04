632ab7a92a25da018e9ee9b04daa218e24ffb045
kazirahiv <rahivjobs@gmail.com>
2018-04-05 02:46:36 +0600

    changes..

--- grab/urls.py
+++ grab/urls.py
@@ -3,7 +3,7 @@ from . import views
 app_name = 'grab'
 urlpatterns= [
 	path('', views.index, name='index'),
-	re_path('grab/(?P<file_name>.+)/$', views.download, name='download'),
+	re_path('(?P<file_name>.+)/$', views.download, name='download'),
 	path('grabStore/', views.grabStore, name='grabStore'),	
 	re_path('grabStore/([A-Za-z0-9_\-]{11})/$', views.grabStoreDownload, name='grabStoreDownload'),
 	re_path('grabStore/([A-Za-z0-9_\-]{11})/(?P<file_name>.+)/$', views.download, name='download'),
