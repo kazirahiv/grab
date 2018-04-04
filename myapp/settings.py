f26a74dfda123793077ecca38d64d5fb07dba9ba
kazirahiv <rahivjobs@gmail.com>
2018-04-05 01:59:42 +0600

    upper

--- myapp/settings.py
+++ myapp/settings.py
@@ -119,6 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_URL='/media/'
-MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
--- myapp/urls.py
+++ myapp/urls.py
@@ -20,4 +20,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls), 
     path('', include('grab.urls')),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
